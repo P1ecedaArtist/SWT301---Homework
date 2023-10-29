b5ec8cc6097c34da7b705b80817957b491b67166
P1ecedaArtist <121916229+P1ecedaArtist@users.noreply.github.com>
2023-10-29 21:11:42 +0700

    TC02Test.java

--- selenium-webdriver-java-master_DaoPhucBinh/src/test/java/BaiTap/TC02Test.java
+++ selenium-webdriver-java-master_DaoPhucBinh/src/test/java/BaiTap/TC02Test.java
@@ -6,6 +6,24 @@ import org.openqa.selenium.WebDriver;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+/*
+
+Test Steps:
+
+1. Goto http://live.techpanda.org/
+
+2. Click on �MOBILE� menu
+
+3. In the list of all mobile , read the cost of Sony Xperia mobile (which is $100)
+
+4. Click on Sony Xperia mobile
+
+5. Read the Sony Xperia mobile from detail page.
+
+6. Compare Product value in list and details page should be equal ($100).
+
+*/
+
 public class TC02Test {
     @Test
     public static void testTC02(){
